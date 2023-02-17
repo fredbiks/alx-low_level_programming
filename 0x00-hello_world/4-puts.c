9f6879c2b724eff29578cb68b45df528a2a96c08
fredbiks <bikundo025@gmail.com>
2023-02-17 12:33:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,7 @@
  */
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle;
-	
-	puts(str);
-	
-	return (0);
-}
+    puts("\"Programming is like building a multilingual puzzle");
 
+    return (0);
+}
