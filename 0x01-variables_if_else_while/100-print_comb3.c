699d8ebe799442ad30ac69559919d21d0eef67e0
fredbiks <bikundo025@gmail.com>
2023-02-19 15:00:42 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
- * using the smallest combination of three digits
- *
- * Return: 0 if program completes successfully
- */
+* main - prints all possible different combinations of two digits
+* Return: ALways 0 (Success)
+*/
 int main(void)
 {
-int i, j, k;
+int n, m;
 
-for (i = 0; i < 8; i++)
+for (n = 48; n <= 56; n++)
 {
-for (j = i + 1; j < 9; j++)
+for (m = 49; m <= 57; m++)
 {
-for (k = j + 1; k <= 9; k++)
+if (m > n)
 {
-putchar(i + '0');
-putchar(j + '0');
-putchar(k + '0');
-
-if (i != 7 || j != 8 || k != 9)
+putchar(n);
+putchar(m);
+if (n != 56 || m != 57)
 {
 putchar(',');
 putchar(' ');
@@ -28,7 +24,6 @@ putchar(' ');
 }
 }
 }
-printf("\n");
-
+putchar('\n');
 return (0);
 }
