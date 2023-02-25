1ab77bfb2d1a249d3e63a717154b39e60508f1d8
fredbiks <bikundo025@gmail.com>
2023-02-25 12:04:52 -0800

    0-main.c,1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates an infinite loop.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+ * while (i < 10)
+	{
+putchar(i);
+	}
+*/
+
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
+
