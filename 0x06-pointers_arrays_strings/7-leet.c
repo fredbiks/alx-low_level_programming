2b5e93571206c8980fff3db21241505109da7418
fredbiks <bikundo025@gmail.com>
2023-03-02 11:51:55 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+/**
+ * leet - Encodes a string into 1337
+ * @str: Pointer to the string to encode
+ *
+ * Return: A pointer to the encoded string
+ */
+char *leet(char *str)
+{
+	char *ptr = str;
+	char *leet_letters = "aAeEoOtTlL";
+	char *leet_numbers = "4433007711";
+	int i, j;
+
+	while (*ptr)
+	{
+		i = 0;
+		while (leet_letters[i])
+		{
+			if (*ptr == leet_letters[i])
+			{
+				*ptr = leet_numbers[i];
+				break;
+			}
+			i++;
+		}
+		ptr++;
+	}
+
+	return (str);
+}
+
