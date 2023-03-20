6f6d1984b5bcf92fcd506f6cd27744e81f939b1e
fredbiks <bikundo025@gmail.com>
2023-03-20 15:54:02 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,23 @@
 #include "dog.h"
 
-void init_dog(struct dog *d, char *name, float age, char *owner) {
-d->name = name;
-d->age = age;
-d->owner = owner;
+/**
+ * init_dog - initializes a dog structure
+ * @d: pointer to dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: This function initializes a dog structure with the given
+ *              name, age, and owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
+
 
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog dog_t;
-
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - struct for dog info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that contains name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
