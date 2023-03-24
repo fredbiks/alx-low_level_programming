e4ef60e63e0d65c92f612cd9080eba7432dff0cc
fredbiks <bikundo025@gmail.com>
2023-03-24 16:41:42 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,11 +3,22 @@
 #include "3-calc.h"
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array of pointers to the arguments
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: Always 0
+ * Return: 0 on success, 98, 99, or 100 on failure
  */
-int main(int argc, char *argv
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+int (*op_func)(int, int);
 
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+
+num1 = atoi(argv
+}
